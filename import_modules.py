21e1b77600d1336b1ff18ce984be8e70dc30ed15
preritaagarwal <55796507+preritaagarwal@users.noreply.github.com>
2022-07-14 11:05:35 +0100

    Add files via upload

---
+++ import_modules.py
@@ -0,0 +1,26 @@
+import xarray as xr
+import salem
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import cartopy.crs as ccrs
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+import cartopy.feature as cfeature
+import matplotlib as mpl
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+from matplotlib.ticker import StrMethodFormatter
+import glob
+import os
+import matplotlib
+from matplotlib.ticker import StrMethodFormatter
+import seaborn as sns
+from scipy import stats
+import matplotlib.lines as mlines
+sns.set_theme()
+import seaborn as sns; sns.set_theme(color_codes=True)
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+import matplotlib.transforms as mtransforms
